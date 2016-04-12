0a4cecb39206f1b1d7fd221886c3f373d9c1f738
Andrei Scopenco <ascopenco@parallels.com>
2016-04-12 15:23:14 +0300

    bump ver

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'akhadiev@parallels.com'
 license 'Apache2'
 description 'Provides LWRP resources to manage mongodb3'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.2.0'
 source_url 'https://git.prls.net/scm/cook/mongodb3-objects.git'
 issues_url ''
 
