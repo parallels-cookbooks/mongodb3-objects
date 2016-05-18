7328ecf0b34e9d0eaea0b61063a809708e17c802
Azat Khadiev <akhadiev@parallels.com>
2016-05-18 10:51:06 +0300

    Version bumped to 0.3.0

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+## 0.3.0 (May 18, 2016)
+  - Add support to add replica set through `mongodb_shard` resource. This breaks backward compatibility for this resource!
+  - Add Centos 7 to converge and verify platforms.
+
+## 0.2.2 (April 14, 2016)
+  - Fix roles for `mongodb_admin` resource.
+
+## 0.2.0 (April 12, 2016)
+  - Add replica set LWRP with tests;
+  - Add sharding LWRPs.
+
+## 0.1.0 (March 24, 2016)
+  - Initial cookbook.
--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 name 'mongodb3-objects'
 maintainer 'The Authors'
 maintainer_email 'akhadiev@parallels.com'
-license 'Apache2'
+license 'Apache 2.0'
 description 'Provides LWRP resources to manage mongodb3'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.2'
+version '0.3.0'
 source_url 'https://git.prls.net/scm/cook/mongodb3-objects.git'
 issues_url ''
 
 depends 'mongodb3', '~> 5.0'
-depends 'mongo_chef_gem', '~> 0.1.0'
+depends 'mongo_chef_gem', '~> 0.1'
